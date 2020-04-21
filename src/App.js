af7860c36da04e46b8ca759e2b68cdc8fac7e9b2
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-20 17:54:29 -0700

    cleanup backticks on Home component

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ import Header from './Header.js';
 import Home from './Home.js';
 import About from './About.js';
 import Footer from './Footer.js';
-<<<<<<< HEAD
-// import Confirmation from './Confirmation.js';
-// import Unsubscribe from './Unsubscribe.js'
-=======
 import Confirmation from './Confirmation.js';
 import Unsubscribe from './Unsubscribe.js'
->>>>>>> 2701220fa7e54eed6367f7d02ba9b4e2be032bce
 
 export default class App extends Component {
   state = { user: null };
@@ -26,11 +21,7 @@ export default class App extends Component {
     return (
       <div>
         <Router>
-<<<<<<< HEAD
-           <Header />
-=======
           <Header />
->>>>>>> 2701220fa7e54eed6367f7d02ba9b4e2be032bce
             <Switch>
               <Route exact path='/' render={(props) => <Home {...props} setUser={ this.setUser } user={ this.state.user }/>}/> 
               <Route exact path='/about' component={About} />
--- src/Unsubscribe.js
+++ src/Unsubscribe.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-=======
->>>>>>> 2701220fa7e54eed6367f7d02ba9b4e2be032bce
 
 export default class Unsubscribe extends Component {
   render() {
