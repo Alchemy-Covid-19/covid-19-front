f786e07ce5c526faa3a0740348d5e3ade360c479
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-20 17:31:58 -0700

    add language to Unsub and Confirm

---
+++ src/Confirmation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Unsubscribe extends Component {
+    render() {
+      return (    
+        <div>
+          <p>Thanks for subscribing to Social Distance-Ping! You will now receive a daily text at 5pm PST with statistics for the location you chose.</p>
+          <p>If you ever wish to unsubscribe, text STOP to +1 309-408-0627 and we will remove you from our database.</p>
+        </div>
+        )
+    }
+}
--- src/Unsubscribe.js
+++ src/Unsubscribe.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
-import request from 'superagent';
 
 export default class Unsubscribe extends Component {
     render() {
         return (
     
           <div>
-              <h3>We are sorry to see you go</h3>
+              <h3>We are sorry to see you go--Thanks for using Social Distance-Ping.</h3>
           </div>
         )
       }
